d9133c4e6df3568c575109d8c266ea34bff1ffee
Parinya Yodming <sqweezpond>
2018-08-10 15:33:23 +0700

    update
    update (pond)

---
+++ inc/header.php
@@ -0,0 +1,34 @@
+    <title>BB Rental</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="robots" content="index, follow, all">
+    <meta name="author" content="workbythai">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- ICON -->
+    <link rel="shortcut icon" href="assets/images/logo/LOGO-BB.png" width="100" height="100" type="image/x-icon"/>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/vandor/animate.css-master/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="node_modules/font-awesome/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+    <!-- Carousel -->
+    <link rel="stylesheet" href="assets/vandor/OwlCarousel/dist/assets/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="assets/vandor/OwlCarousel/dist/assets/owl.theme.default.min.css">
+
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Kanit:300,400,500,700" rel="stylesheet">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="font-icon/flaticon.css">
---
+++ inc/navbar.php
@@ -0,0 +1,149 @@
+
+
+<!--- NAVBAR 1 ------------------------------------------------->
+	<div class="nav-bg d-none d-lg-block d-md-none">
+		   <div class="container">
+						<div class="row">
+								 <div class="col-xl-3 col-lg-3 col-">
+									  <a href="#">
+												<div class="nav-overlay-image">
+														<img src="assets/images/logo/LOGO-BB.png" class="nav-image-full">
+												</div>
+										</a>
+									</div>
+									<div class="col-xl-6 col-lg-6 text-center">
+											<div class="nav-overlay-image2">
+													<img src="assets/images/logo/FONT-BB.png" class="nav-image-full">
+											</div>
+									</div>
+									<div class="col-xl-3 col-lg-3 text-right">
+												<div class="btn-group nav-p-t" role="group" aria-label="">
+													 <button type="button" class="btn nav-btn-lang nav-btn-active">ไทย</button>
+													 <button type="button" class="btn nav-btn-lang">Eng</button>
+												</div>
+												<div class="mt-3">
+														<div class="nav-font3"><i class="nav-icon fa fa-volume-control-phone" aria-hidden="true"></i>ติดต่อสอบถาม</div>
+														<div class="nav-font3">038-451556</div>
+												</div>
+									</div>
+						</div>
+				</div>
+	</div>
+<!-- END NAVBAR 1-->
+
+
+<!-- NAVBAR 2 ---------------------------------------------------->
+<div class="nav-bg2 d-none d-lg-block d-md-none">
+		<div class="container">
+				<div class="row">
+						<div class="col-xl-9 col-lg-9">
+							  <span class="nav-font">MACHINE FOR RENT</span>
+								<span class="nav-font2 ml-3">บริการให้เช่าเครื่องจักรสำหรับงานก่อสร้างทุกชนิด</span>
+						</div>
+						<div class="col-xl-3 col-lg-3">
+								<div class="input-group">
+									  <input type="text" class="form-control nav-search-form" placeholder="ค้นหาครื่องจักร">
+								</div>
+						</div>
+				</div>
+		</div>
+</div>
+<!-- END NAVBAR 2 ---------------------------->
+
+
+<!-- NAVBAR 3 ---------------------------->
+	<div class="nav-bg3 d-none d-lg-block d-md-none">
+			<div class="container">
+					<div class="row">
+							<div class="navbar px-0">
+								  <a href="#">หน้าหลัก</a>
+								  <a href="#" class="nav-m-l">เกี่ยวกับบริษัท</a>
+									<a href="#" class="nav-m-l">เงื่อนไขการเช่า</a>
+									<a href="#" class="nav-m-l">ประวัติการเช่า</a>
+								  <div class="dropdown nav-m-l">
+									    <button class="dropbtn">เครื่องจักรสำหรับเช่า</button>
+									    <div class="dropdown-content">
+									      <a href="#">รถกระเช้าแบบบูมพับ</a>
+									      <a href="#">รถกระเช้าแบบบูมตรง</a>
+									      <a href="#">รถกระเช้าแบบกรรไกร</a>
+												<a href="#">รถขุด / รถแบ็คโฮ</a>
+												<a href="#">รถบด</a>
+												<a href="#">รถเกรด</a>
+												<a href="#">รถบรรทุกติดตั้งเครน</a>
+												<a href="#">รถขนส่ง</a>
+												<a href="#">รถกระเช้า</a>
+									    </div>
+								  </div>
+									<a href="#" class="nav-m-l">ฝ่ายบริการ</a>
+									<a href="#" class="nav-m-l">ติดต่อเรา</a>
+							</div>
+					</div>
+			</div>
+	</div>
+<!-- END NAVBAR 3 ---------------------------->
+
+
+	<div class="d-block d-md-block d-lg-none">
+			<nav class="navbar navbar-expand-lg nav-bg row mx-0 px-0">
+					<div class="col-6">
+						  <a href="#">
+								<div class="nav-overlay-image">
+										<img src="assets/images/logo/LOGO-BB.png" class="nav-image-full">
+								</div>
+							</a>
+					</div>
+					<div class="col-6 text-right">
+						  <button class="navbar-toggler hamburger-btn" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						    <i class="fa fa-bars" aria-hidden="true"></i>
+						  </button>
+					</div>
+						  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+								    <ul class="navbar-nav">
+									      <li class="nav-item">
+									         <a class="nav-link" href="#">หน้าหลัก</a>
+									      </li>
+									      <li class="nav-item">
+									         <a class="nav-link" href="#">เกี่ยวกับบริษัท</a>
+									      </li>
+									      <li class="nav-item">
+									         <a class="nav-link" href="#">เงื่อนไขการเช่า</a>
+									      </li>
+												<li class="nav-item">
+									         <a class="nav-link" href="#">ประวัติการเช่า</a>
+									      </li>
+												<li class="nav-item dropdown">
+														<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+															เครื่องจักรสำหรับเช่า
+														</a>
+														<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+																 <a class="dropdown-item" href="#">รถกระเช้าแบบบูมพับ</a>
+																 <a class="dropdown-item" href="#">รถกระเช้าแบบบูมตรง</a>
+																 <a class="dropdown-item" href="#">รถกระเช้าแบบกรรไกร</a>
+																 <a class="dropdown-item" href="#">รถขุด / รถแบ็คโฮ</a>
+																 <a class="dropdown-item" href="#">รถบด</a>
+																 <a class="dropdown-item" href="#">รถเกรด</a>
+																 <a class="dropdown-item" href="#">รถบรรทุกติดตั้งเครน</a>
+																 <a class="dropdown-item" href="#">รถขนส่ง</a>
+																 <a class="dropdown-item" href="#">รถกระเช้า</a>
+														</div>
+												</li>
+												<li class="nav-item">
+													 <a class="nav-link" href="#">ฝ่ายบริการ</a>
+											  </li>
+											  <li class="nav-item">
+													 <a class="nav-link" href="#">ติดต่อเรา</a>
+											  </li>
+												<li class="nav-item">
+													<div class="btn-group nav-p-t" role="group" aria-label="">
+														 <button type="button" class="btn nav-btn-lang nav-btn-active">ไทย</button>
+														 <button type="button" class="btn nav-btn-lang">Eng</button>
+													</div>
+													<div class="mt-3">
+															<span class="nav-font3"><i class="nav-icon fa fa-volume-control-phone" aria-hidden="true"></i>ติดต่อสอบถาม :</span>
+															<span class="nav-font3">038-451556</span>
+													</div>
+									      </li>
+								    </ul>
+						  </div>
+			</nav>
+	</div>
